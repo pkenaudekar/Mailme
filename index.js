5876b289ef198010d19ecb8bb191574ea55bae42
Pratik Kenaudekar <scorp86@gmail.com>
2020-12-10 22:34:41 +0530

    Dependency fixes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
-const boayParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 require('./models/User');
 require('./models/Survey');
@@ -11,38 +11,38 @@ require('./services/passport');
 
 const app = express();
 
-app.use(boayParser.json());
+app.use(bodyParser.json());
 
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60* 60 * 1000, // cookie lasts for 30 days before it expires.
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // cookie lasts for 30 days before it expires.
+    keys: [keys.cookieKey],
+  })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.connect(keys.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true    
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
-if(process.env.NODE_ENV  === 'production'){
-    // Express will surve up production asssets 
-    // like our main.js file, or main.css files!
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  // Express will surve up production asssets
+  // like our main.js file, or main.css files!
+  app.use(express.static('client/build'));
 
-    // Express will serve up the insex.html file
-    // if it doesnt't recognize the route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  // Express will serve up the index.html file
+  // if it doesnt't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
